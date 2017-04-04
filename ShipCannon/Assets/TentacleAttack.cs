c67c845cfe18ea7d396cb5fe74ccb455ec6bb2bb
GatorSutton <garrettsuttonee@gmail.com>
2017-04-04 19:07:37 -0400

    TentacleWork

--- ShipCannon/Assets/TentacleAttack.cs
+++ ShipCannon/Assets/TentacleAttack.cs
@@ -1,41 +1,69 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TentacleAttack : MonoBehaviour {
+public class TentacleAttack : MonoBehaviour
+{
 
     public float m_Speed;
 
     private Quaternion targetRotation;
     private Quaternion startRotation;
-    private Quaternion resetRotation;
-    private float x =1;
+    private float x = 1;
     private float z = 1;
+    private float t = 0;
 
-	// Use this for initialization
-	void Start () {
+    enum states
+    {
+        idle,
+        attacking
+    }
+
+    // Use this for initialization
+    void Start()
+    {
         startRotation = transform.rotation;
         targetRotation = Quaternion.AngleAxis(90, new Vector3(x, 0, z)) * startRotation;
-        resetRotation = Quaternion.AngleAxis(90, new Vector3(0, 1, 0));
+    }
 
-        InvokeRepeating("Switch", 5, 5);
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
 
 
         //transform.Rotate(new Vector3(x, 0, z), Time.deltaTime);
 
-        transform.rotation = Quaternion.Lerp(startRotation, targetRotation, Time.time * m_Speed);
+        transform.rotation = Quaternion.Lerp(startRotation, targetRotation, t * m_Speed);
+
+
+        
+        if (Input.GetButtonDown("Shoot"))
+        {
+            t = 0;
+            x = Random.Range(-1f, 1f);
+            z = Random.Range(-1f, 1f);
+            targetRotation = Quaternion.AngleAxis(90, new Vector3(x, 0f, z)) * startRotation;
+            
+
+
+        }
+
+
+        t += Time.deltaTime;
 
 
 
     }
+}
 
-    void Switch()
-    {
-        targetRotation = resetRotation;
-    }
+
+    
+
+
+
+
+   
+
+
 
  
     
@@ -45,4 +73,4 @@ public class TentacleAttack : MonoBehaviour {
 
 
  
-}
+
