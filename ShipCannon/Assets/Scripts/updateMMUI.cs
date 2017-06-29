d9057207bf467e2376d2474c1c3cd1cbb58a4f47
GatorSutton <garrettsuttonee@gmail.com>
2017-06-28 20:09:52 -0400

    2 monitors title screen

--- ShipCannon/Assets/Scripts/updateMMUI.cs
+++ ShipCannon/Assets/Scripts/updateMMUI.cs
@@ -7,6 +7,8 @@ public class updateMMUI : MonoBehaviour {
 
     public Text coinsNeeded;
     public Text credits;
+    public Text coinsNeeded2;
+    public Text credits2;
     public float speed;
 
     InsertCoins IC;
@@ -25,13 +27,17 @@ public class updateMMUI : MonoBehaviour {
     {
         credits.text = "Credits: " + IC.credits;
         coinsNeeded.text = "Insert " + IC.coinsNeeded + " coins";
+        credits2.text = "Credits: " + IC.credits;
+        coinsNeeded2.text = "Insert " + IC.coinsNeeded + " coins";
 
-        if(IC.credits > 0)
+        if (IC.credits > 0)
         {
             coinsNeeded.text = "PRESS ANY BUTTON TO START";
+            coinsNeeded2.text = "PRESS ANY BUTTON TO START";
             Color color = credits.color;
             color.a = Mathf.Round(Mathf.PingPong(Time.time * speed, 1f));
             coinsNeeded.color = color;
+            coinsNeeded2.color = color;
         }
     }
 }
