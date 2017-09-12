959b787dc7777eac24481930c72923982aa21eb9
GatorSutton <garrettsuttonee@gmail.com>
2017-09-12 12:17:59 -0400

    Pursuit Implemented
    Needs direction check and lerping

---
+++ ShipCannon/Assets/Pursuit.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pursuit : MonoBehaviour {
+
+    private ShipControl SC;
+    public float pursuitSpeed = 10;
+    public float normalSpeed = 5;
+
+	// Use this for initialization
+	void Start () {
+        SC = GetComponent<ShipControl>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "pursuitangle")
+        {
+            SC.m_Speed = pursuitSpeed;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "pursuitangle")
+        {
+            SC.m_Speed = normalSpeed;
+        }
+    }
+
+
+}
